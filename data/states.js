c83a1a958929c5b1e0c4f19ec764873758216847
ineshyk <ineshyk@gmail.com>
2017-11-06 09:50:48 +0200

    new version 1.5

---
+++ data/states.js
@@ -0,0 +1,52 @@
+'use strict';
+var Mockgen = require('./mockgen.js');
+/**
+ * Operations on /states
+ */
+module.exports = {
+    /**
+     * summary: Post states by the waybills
+     * description: By passing in the appropriate options, you can post
+states for the waybills
+
+     * parameters: States
+     * produces: application/json
+     * responses: 201, 400, 409
+     * operationId: postStates
+     */
+    post: {
+        201: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/states',
+                operation: 'post',
+                response: '201'
+            }, callback);
+        },
+        400: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/states',
+                operation: 'post',
+                response: '400'
+            }, callback);
+        },
+        409: function (req, res, callback) {
+            /**
+             * Using mock data generator module.
+             * Replace this by actual data for the api.
+             */
+            Mockgen().responses({
+                path: '/states',
+                operation: 'post',
+                response: '409'
+            }, callback);
+        }
+    }
+};
---
+++ handlers/states.js
@@ -0,0 +1,31 @@
+'use strict';
+var dataProvider = require('../data/states_dev.js');
+/**
+ * Operations on /states
+ */
+module.exports = {
+    /**
+     * summary: Post states by the waybills
+     * description: By passing in the appropriate options, you can post
+states for the waybills
+
+     * parameters: States
+     * produces: application/json
+     * responses: 201, 400, 409
+     */
+    post: function postStates(req, res, next) {
+        /**
+         * Get the data for response 201
+         * For response `default` status 200 is used.
+         */
+        var status = 201;
+        var provider = dataProvider['post']['201'];
+        provider(req, res, function (err, data) {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.status(status).send(data && data.responses);
+        });
+    }
+};
