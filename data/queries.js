05eedf6e3fe310c68bf8e0b26fb08cdb824c7941
iNeshyk <ineshyk@gmail.com>
2018-03-23 15:05:05 +0200

    fix contractor FormID

--- data/queries.js
+++ data/queries.js
@@ -193,7 +193,7 @@ module.exports = {
     INSERT INTO t005_UserData
     SELECT DISTINCT
       uc.UserID AS UserID,
-      jt.EntryGUID AS EntryGUID,
+      jt.FormID AS EntryGUID,
       1 AS Active,
       'L' AS RecordType
     FROM OPENJSON(@json1) WITH (EntryGUID char(36), Sha1KeyValue varchar(50)) AS jt
@@ -259,7 +259,8 @@ module.exports = {
               INSERT (${colsLines.join(',')}) VALUES (${valsLines.join(',')});`;
 
     let q_addLabAnalysis = q1_addLabAnalysis+'\n  \n'+q2_addLabAnalysis+'\n  \n'+q3_addLabAnalysis;
-
+    //let q_addLabAnalysis = q1_addLabAnalysis+'\n  \n'+q2_addLabAnalysis;
+    console.log(q_addLabAnalysis);
     return q_addLabAnalysis;
 
   },
